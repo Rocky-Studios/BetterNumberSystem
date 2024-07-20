66ee1f0af396647feaf8807ea27b7ff1118a0e44
Gergo Kaszas <gergo.kaszas@outlook.com>
2024-07-21 09:06:24 +1000

    Extract number units from list and made them static members

--- NumberUnit.cs
+++ NumberUnit.cs
@@ -30,6 +30,15 @@
         /// </summary>
         public bool CanBeNegative { get; set; }
         public MeasurementType MeasurementType;
+        /// <summary>
+        /// Initialises a number unit
+        /// </summary>
+        /// <param name="fullName">The full name</param>
+        /// <param name="suffix">The suffix after a number</param>
+        /// <param name="measurementType">Which category of measurement it goes into</param>
+        /// <param name="baseUnit">Whether this is the standard base unit used in conversions</param>
+        /// <param name="proportionalToBaseUnit">How many base units equal one of these</param>
+        /// <param name="canBeNegative">Whether this value can be negative</param>
         public NumberUnit(string fullName, string suffix, MeasurementType measurementType, bool baseUnit = false, double proportionalToBaseUnit = 1, bool canBeNegative = false)
         {
             FullName = fullName;
@@ -42,57 +51,222 @@
             if (_numberUnits == null) _numberUnits = new List<NumberUnit>();
             _numberUnits.Add(this);
         }
+        /// <summary>
+        /// A number with no unit
+        /// </summary>
+        public static readonly NumberUnit PLAIN = new NumberUnit("Plain", "", MeasurementType.Plain, baseUnit: true, canBeNegative: true);
+
+        #region Length
+        /// <summary>
+        /// Millimetre (mm) <br/>
+        /// 1000th of a metre
+        /// </summary>
+        public static readonly NumberUnit MILLIMETRE = new NumberUnit("Millimetre", "mm", MeasurementType.Length, proportionalToBaseUnit: 1f/1000f);
+        /// <summary>
+        /// Centimetre (cm) <br/>
+        /// 100th of a metre
+        /// </summary>
+        public static readonly NumberUnit CENTIMETRE = new NumberUnit("Centimetre", "cm", MeasurementType.Length, proportionalToBaseUnit: 1f/100f);
+        /// <summary>
+        /// <strong> SI Base Unit </strong> Metre (m) <br/>
+        /// The length of the path travelled by light in vacuum in ⁠1/299792458⁠ of a second
+        /// </summary>
+        public static readonly NumberUnit METRE = new NumberUnit("Metre", "m", MeasurementType.Length, baseUnit: true);
+        /// <summary>
+        /// Kilometre (Km) <br/>
+        /// 1000 metres
+        /// </summary>
+        public static readonly NumberUnit KILOMETRE = new NumberUnit("Kilometre", "Km", MeasurementType.Length, proportionalToBaseUnit: 1000f);
+        #endregion
+
+        #region Area
+        /// <summary>
+        /// Square millimetre (mm²) <br/>
+        /// The area of a square with edges one millimetre in length.
+        /// </summary>
+        public static readonly NumberUnit SQUARE_MILLIMETRE = new NumberUnit("SqMillimetre", "mm²", MeasurementType.Area, proportionalToBaseUnit: 1f/1000000f);
+        /// <summary>
+        /// Square centimetre (cm²) <br/>
+        /// The area of a square with edges one centimetre in length.
+        /// </summary>
+        public static readonly NumberUnit SQUARE_CENTIMETRE = new NumberUnit("SqCentimetre", "cm²", MeasurementType.Area, proportionalToBaseUnit: 1f/10000f);
+        /// <summary>
+        /// <strong>SI Base Unit</strong> Square metre (m²) <br/>
+        /// The area of a square with edges one metre in length.
+        /// </summary>
+        public static readonly NumberUnit SQUARE_METRE = new NumberUnit("SqMetre", "m²", MeasurementType.Area, baseUnit: true);
+        /// <summary>
+        /// Square kilometre (Km²) <br/>
+        /// The area of a square with edges one kilometre in length.
+        /// </summary>
+        public static readonly NumberUnit SQUARE_KILOMETRE = new NumberUnit("SqKilometre", "Km²", MeasurementType.Area, proportionalToBaseUnit: 1000000f);
+        /// <summary>
+        /// Hectare (ha) <br/>
+        /// The area of a square with edges 100 metres in length.
+        /// </summary>
+        public static readonly NumberUnit HECTARE = new NumberUnit("Hectare", "ha", MeasurementType.Area, proportionalToBaseUnit: 10000f);
+        #endregion
+
+        #region Volume
+        /// <summary>
+        /// Cubic millimetre (mm³) <br/>
+        /// The volume of a cube with edges one millimetre in length.
+        /// </summary>
+        public static readonly NumberUnit CUBIC_MILLIMETRE = new NumberUnit("CuMillimetre", "mm³", MeasurementType.Volume, proportionalToBaseUnit: 1f/1000000000f);
+        /// <summary>
+        /// Cubic centimetre (cm³) <br/>
+        /// The volume of a cube with edges one centimetre in length.
+        /// </summary>
+        public static readonly NumberUnit CUBIC_CENTIMETRE = new NumberUnit("CuCentimetre", "cm³", MeasurementType.Volume, proportionalToBaseUnit: 1f/1000000f);
+        /// <summary>
+        /// <strong>SI Base Unit</strong> Cubic metre (m³)
+        /// The volume of a cube with edges one metre in length.
+        /// </summary>
+        public static readonly NumberUnit CUBIC_METRE = new NumberUnit("CuMetre", "m³", MeasurementType.Volume, baseUnit: true);
+        /// <summary>
+        /// Cubic kilometre (Km³) <br/>
+        /// The volume of a cube with edges one kilometre in length.
+        /// </summary>
+        public static readonly NumberUnit CUBIC_KILOMETRE = new NumberUnit("CuKilometre", "Km³", MeasurementType.Volume, proportionalToBaseUnit: 1000000000f);
+        /// <summary>
+        /// Millilitre (mL)<br/>
+        /// 1000 mL = 1 L
+        /// </summary>
+        public static readonly NumberUnit MILLILITRE = new NumberUnit("Millilitre", "mL", MeasurementType.Volume, proportionalToBaseUnit: 1f/1000000f);
+        /// <summary>
+        /// Litre (L)<br/>
+        /// 1000 L = 1 m³
+        /// </summary>
+        public static readonly NumberUnit LITRE = new NumberUnit("Litre", "L", MeasurementType.Volume, proportionalToBaseUnit: 1f/1000f);
+        /// <summary>
+        /// Kilolitre (KL)<br/>
+        /// KL = m³, 1 KL = 1000 L
+        /// </summary>
+        public static readonly NumberUnit KILOLITRE = new NumberUnit("Kilolitre", "KL", MeasurementType.Volume, proportionalToBaseUnit: 1f);
+        #endregion
+
+        #region Time
+        /// <summary>
+        /// Millisecond (ms) <br/>
+        /// 1000th of a second
+        /// </summary>
+        public static readonly NumberUnit MILLISECOND = new NumberUnit("Millisecond", "ms", MeasurementType.Time, proportionalToBaseUnit: 1f/1000f);
+        /// <summary>
+        /// <strong>SI Base Unit</strong> Second (s) <br/>
+        /// The second is defined by taking the fixed numerical value of the caesium frequency, ΔνCs, the unperturbed ground-state hyperfine transition frequency of the caesium 133 atom, to be 9192631770 when expressed in the unit Hz, which is equal to s−1.
+        /// </summary>
+        public static readonly NumberUnit SECOND = new NumberUnit("Second", "s", MeasurementType.Time, baseUnit: true);
+        /// <summary>
+        /// Minute (m) <br/>
+        /// 60 seconds
+        /// </summary>
+        public static readonly NumberUnit MINUTE = new NumberUnit("Minute", "m", MeasurementType.Time, proportionalToBaseUnit: 60f);
+        /// <summary>
+        /// Hour (h) <br/>
+        /// 60 minutes / 3600 seconds
+        /// </summary>
+        public static readonly NumberUnit HOUR = new NumberUnit("Hour", "h", MeasurementType.Time, proportionalToBaseUnit: 3600f);
+        /// <summary>
+        /// Day (d) <br/>
+        /// 24 hours
+        /// </summary>
+        public static readonly NumberUnit DAY = new NumberUnit("Day", "d", MeasurementType.Time, proportionalToBaseUnit: 60f * 60f * 24f);
+        /// <summary>
+        /// Week (w) <br/>
+        /// 7 days
+        /// </summary>
+        public static readonly NumberUnit WEEK = new NumberUnit("Week", "w", MeasurementType.Time, proportionalToBaseUnit: 60f * 60f * 24f * 7f);
+        /// <summary>
+        /// Year (y) <br/>
+        /// <strong>NOTE: </strong> A year here is defined as 365 days
+        /// </summary>
+        public static readonly NumberUnit YEAR = new NumberUnit("Year", "y", MeasurementType.Time, proportionalToBaseUnit: 60f * 60f * 24f * 365f);
+        /// <summary>
+        /// Decade (dy) <br/>
+        /// 10 years
+        /// </summary>
+        public static readonly NumberUnit DECADE = new NumberUnit("Decade", "dy", MeasurementType.Time, proportionalToBaseUnit: 60f * 60f * 24f * 365f * 10f);
+        /// <summary>
+        /// Century (cy) <br/>
+        /// 100 years
+        /// </summary>
+        public static readonly NumberUnit CENTURY = new NumberUnit("Century", "dy", MeasurementType.Time, proportionalToBaseUnit: 60f * 60f * 24f * 365f * 100f);
+        /// <summary>
+        /// Millenium (my) <br/>
+        /// 1000 years
+        /// </summary>
+        public static readonly NumberUnit MILLENIUM = new NumberUnit("Millenium", "my", MeasurementType.Time, proportionalToBaseUnit: 60f * 60f * 24f * 365f * 1000f);
+        #endregion
+
+        #region Mass
+        /// <summary>
+        /// Milligram (mg) <br/>
+        /// 1000th of a gram
+        /// </summary>
+        public static readonly NumberUnit MILLIGRAM = new NumberUnit("Milligram", "mg", MeasurementType.Mass, proportionalToBaseUnit: 1f/1000f);
+        /// <summary>
+        /// Gram (g) <br/>
+        /// 1000th of a kilogram
+        /// </summary>
+        public static readonly NumberUnit GRAM = new NumberUnit("Gram", "g", MeasurementType.Mass, baseUnit: true);
+        /// <summary>
+        /// <strong>SI Base Unit</strong> Kilogram (kg) <br/>
+        /// The mass of 1 litre of pure water.
+        /// </summary>
+        public static readonly NumberUnit KILOGRAM = new NumberUnit("Kilogram", "Kg", MeasurementType.Mass, proportionalToBaseUnit: 1000f);
+        /// <summary>
+        /// Tonne (t) <br/>
+        /// 1000 kilograms
+        /// </summary>
+        public static readonly NumberUnit TONNE = new NumberUnit("Tonne", "t", MeasurementType.Mass, proportionalToBaseUnit: 1000000f);
+        #endregion
+
+        #region Angle
+        /// <summary>
+        /// <strong>SI Base Unit</strong> Radian (rad) <br/>
+        /// An arc of a circle with the same length as the radius of that circle subtends an angle of 1 radian. The circumference subtends an angle of 2π radians.
+        /// </summary>
+        public static readonly NumberUnit RADIAN = new NumberUnit("Radian", "rad", MeasurementType.Angle, baseUnit: true, canBeNegative: true);
+        /// <summary>
+        /// Degree of an arc (°) <br/>
+        /// 1/360th of a full revolution around a circle
+        /// </summary>
+        public static readonly NumberUnit DEGREE = new NumberUnit("Degree", "°", MeasurementType.Angle, proportionalToBaseUnit: MathF.PI / 180, canBeNegative: true);
+        #endregion
+
+        #region Energy
+        /// <summary>
+        /// <strong>SI Base Unit</strong> Joule (J)<br/>
+        /// The work required to produce one watt of power for one second, or one watt-second (W⋅s) (compare kilowatt-hour, which is 3.6 megajoules).
+        /// </summary>
+        public static readonly NumberUnit JOULE = new NumberUnit("Joule", "j", MeasurementType.Energy, baseUnit: true);
+        /// <summary>
+        /// Kilojoule (KJ)<br/>
+        /// 1000 joules
+        /// </summary>
+        public static readonly NumberUnit KILOJOULE = new NumberUnit("Kilojoule", "Kj", MeasurementType.Energy, proportionalToBaseUnit: 1000f);
+        /// <summary>
+        /// Megajoule (MJ)<br/>
+        /// 1000000 joules
+        /// </summary>
+        public static readonly NumberUnit MEGAJOULE = new NumberUnit("Megajoule", "Mj", MeasurementType.Energy, proportionalToBaseUnit: 1000000f);
+        /// <summary>
+        /// Calorie (cal)<br/>
+        /// 1000th of a kilocalorie
+        /// </summary>
+        public static readonly NumberUnit CALORIE = new NumberUnit("Calorie", "cal", MeasurementType.Energy, proportionalToBaseUnit: 4.184f);
+        /// <summary>
+        /// Kilocalorie (kcal)<br/>
+        /// The amount of (heat) energy needed to raise the temperature of one liter of water by one degree Celsius (or kelvin).
+        /// </summary>
+        public static readonly NumberUnit KILOCALORIE = new NumberUnit("Kilocalorie", "kcal", MeasurementType.Energy, proportionalToBaseUnit: 4184f);
+        #endregion
+
 
         /// <summary>
         /// The list of all number units avaiable for use
         /// </summary>
-        private static List<NumberUnit> _numberUnits = new()
-        {
-            new NumberUnit("Plain", "", MeasurementType.Plain, baseUnit: true, canBeNegative: true),
-            // Length
-            new NumberUnit("Millimetre", "mm", MeasurementType.Length, proportionalToBaseUnit: 1000),
-            new NumberUnit("Centimetre", "cm", MeasurementType.Length, proportionalToBaseUnit: 100),
-            new NumberUnit("Metre", "m", MeasurementType.Length, baseUnit: true),
-            new NumberUnit("Kilometre", "Km", MeasurementType.Length, proportionalToBaseUnit: 0.001f),
-            // Area
-            new NumberUnit("SqMillimetre", "mm²", MeasurementType.Area, proportionalToBaseUnit: 1000000),
-            new NumberUnit("SqCentimetre", "cm²", MeasurementType.Area, proportionalToBaseUnit: 10000),
-            new NumberUnit("SqMetre", "m²", MeasurementType.Area, baseUnit: true),
-            new NumberUnit("SqKilometre", "Km²", MeasurementType.Area, proportionalToBaseUnit: 0.000001f),
-            new NumberUnit("Hectare", "ha", MeasurementType.Area, proportionalToBaseUnit: 0.0001f),
-            // Volume
-            new NumberUnit("CuMillimetre", "mm³", MeasurementType.Volume, proportionalToBaseUnit: 1000000000),
-            new NumberUnit("CuCentimetre", "cm³", MeasurementType.Volume, proportionalToBaseUnit: 1000000),
-            new NumberUnit("CuMetre", "m³", MeasurementType.Volume, baseUnit: true),
-            new NumberUnit("CuKilometre", "km³", MeasurementType.Volume, proportionalToBaseUnit: 0.000000001f),
-            new NumberUnit("Millilitre", "ml", MeasurementType.Volume, proportionalToBaseUnit: 1000000f),
-            new NumberUnit("Litre", "l", MeasurementType.Volume, proportionalToBaseUnit: 1000f),
-            new NumberUnit("Kilolitre", "Kl", MeasurementType.Volume, proportionalToBaseUnit: 1f),
-            // Time
-            new NumberUnit("Millisecond", "ms", MeasurementType.Time, proportionalToBaseUnit: 3600000f),
-            new NumberUnit("Second", "s", MeasurementType.Time, proportionalToBaseUnit: 3600f),
-            new NumberUnit("Minute", "m", MeasurementType.Time, proportionalToBaseUnit: 60f),
-            new NumberUnit("Hour", "h", MeasurementType.Time, baseUnit: true),
-            new NumberUnit("Day", "d", MeasurementType.Time, proportionalToBaseUnit: 1f/24f),
-            new NumberUnit("Week", "w", MeasurementType.Time, proportionalToBaseUnit: 1f/(24f*7f)),
-            new NumberUnit("Year", "y", MeasurementType.Time, proportionalToBaseUnit: 1f/(24f*365f)),
-            new NumberUnit("Decade", "d", MeasurementType.Time, proportionalToBaseUnit: 1f/(24f*365f*10)),
-            new NumberUnit("Century", "c", MeasurementType.Time, proportionalToBaseUnit: 1f /(24f * 365f * 100)),
-            new NumberUnit("Millenium", "m", MeasurementType.Time, proportionalToBaseUnit: 1f /(24f * 365f * 1000)),
-            // Mass
-            new NumberUnit("Milligram", "mg", MeasurementType.Mass, proportionalToBaseUnit: 1000f),
-            new NumberUnit("Gram", "g", MeasurementType.Mass, baseUnit: true),
-            new NumberUnit("Kilogram", "Kg", MeasurementType.Mass, proportionalToBaseUnit: 0.001f),
-            new NumberUnit("Tonne", "t", MeasurementType.Mass, proportionalToBaseUnit: 0.000001f),
-            // Angles
-            new NumberUnit("Radian", "rad", MeasurementType.Angle, proportionalToBaseUnit: MathF.PI/180f, canBeNegative: true),
-            new NumberUnit("Degree", "°", MeasurementType.Angle, baseUnit: true, canBeNegative: true),
-            // Energy
-            new NumberUnit("Joule", "j", measurementType: MeasurementType.Energy, baseUnit: true),
-            new NumberUnit("Kilojoule", "Kj", measurementType: MeasurementType.Energy, proportionalToBaseUnit: 0.001f),
-            new NumberUnit("Megajoule", "Mj", measurementType: MeasurementType.Energy, proportionalToBaseUnit: 0.000001f),
-            new NumberUnit("Calorie", "cal", measurementType: MeasurementType.Energy, proportionalToBaseUnit: 0.23912f),
-        };
+        private static List<NumberUnit> _numberUnits = new();
         /// <summary>
         /// Gets all the currently available number units
         /// </summary>
@@ -103,6 +277,7 @@
         }
         /// <summary>
         /// Query for a numberUnit by its full name
+        /// <br/> Use NumberUnit.XXXXXXXX if possible
         /// </summary>
         /// <param name="fullName"></param>
         /// <returns></returns>
