9697b1a82f979770c56b9eb816d9c12fc4bb26dd
Gergo Kaszas <gergo.kaszas@outlook.com>
2024-08-21 10:19:56 +1000

    Vector and Magnitude

---
+++ Vector.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterNumberSystem
+{
+    public interface IVector
+    {
+        public Number X { get; set; }
+        public Number Y { get; set; }
+
+        public virtual Number GetMagnitude()
+        {
+            Number YConverted = Y.Convert(X.Unit);
+            return new Number(Math.Sqrt(X.NumericValue * X.NumericValue + YConverted.NumericValue * YConverted.NumericValue), X.Unit);
+        }
+    }
+
+    public class Vector2 : IVector
+    {
+        public Number X { get; set; }
+        public Number Y { get; set; }
+
+        public Vector2(Number x, Number y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Number GetMagnitude()
+        {
+            Number YConverted = Y.Convert(X.Unit);
+            return new Number(Math.Sqrt(X.NumericValue * X.NumericValue + YConverted.NumericValue * YConverted.NumericValue), X.Unit);
+        }
+    }
+
+    public class Vector3 : IVector
+    {
+        public Number X { get; set; }
+        public Number Y { get; set; }
+        public Number Z;
+
+        public Vector3(Number x, Number y, Number z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector2 XY() { return new Vector2(X, Y); }
+
+        public Number GetMagnitude()
+        {
+            Number YConverted = Y.Convert(X.Unit);
+            Number ZConverted = Z.Convert(X.Unit);
+            return new Number(Math.Sqrt(
+                X.NumericValue * X.NumericValue +
+                (YConverted.NumericValue * YConverted.NumericValue +
+                ZConverted.NumericValue * ZConverted.NumericValue
+                )), X.Unit);
+        }
+    }
+}
