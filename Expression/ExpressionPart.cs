4e79858bf0b0c36bbf7eb277c015be87591c1e17
Gergo Kaszas <gergo.kaszas@outlook.com>
2024-08-31 20:16:06 +1000

    Pronumerals and expression parts

---
+++ Expression/ExpressionPart.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterNumberSystem.Expression
+{
+    /// <summary>
+    /// The base class for things that make up an expression
+    /// </summary>
+    public interface IExpressionPart
+    {
+
+    }
+    /// <summary>
+    /// The values on which mathematical operations occur in an algebraic expression
+    /// </summary>
+    public class ExpressionTerm : IExpressionPart
+    {
+        public IExpressionValue Value;
+        public List<IPronumeral> Pronumerals;
+    }
+    /// <summary>
+    /// Anything that changes values, +, -, sin, summation, exponents, integrals etc...
+    /// </summary>
+    public class ExpressionFunction : IExpressionPart
+    {
+        /// <summary>
+        /// The full name of the function eg. Sum, Integral
+        /// </summary>
+        public string Name = "";
+        /// <summary>
+        /// A symbol used to identify this function eg. + for sum, ∫ for integral
+        /// </summary>
+        public string? Symbol = "";
+        /// <summary>
+        /// The computations that happen when the function is evaluated
+        /// </summary>
+        public MathFunction Function;
+    }
+    /// <summary>
+    /// The computations that happen when a function is evaluated
+    /// </summary>
+    public delegate IExpressionValue MathFunction(IExpressionValue[] inputs);
+    /// <summary>
+    /// The solver will prioritize things inside this group (basically acts like brackets)
+    /// </summary>
+    public class ExpressionGroup : IExpressionPart
+    {
+        public List<IExpressionPart> Parts = new();
+    }
+}
---
+++ Expression/Pronumeral.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterNumberSystem.Expression
+{
+    public interface IPronumeral
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public IExpressionValue Value { get; set; }
+    }
+
+    public class Variable : IPronumeral
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public IExpressionValue Value { get; set; }
+    }
+
+    public class Constant : IPronumeral
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public IExpressionValue Value { get; set; }
+        /// <summary>
+        /// A circle's circumference divided by its diameter
+        /// </summary>
+        public static Constant PI = new Constant()
+        {
+            Name = "pi",
+            Symbol = "π",
+            Value = new Number(MathF.PI)
+        };
+        /// <summary>
+        /// The base of the natural logarithm
+        /// </summary>
+        public static Constant E = new Constant()
+        {
+            Name = "e",
+            Symbol = "e",
+            Value = new Number(MathF.E)
+        };
+    }
+}
