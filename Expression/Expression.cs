aef5140fc17ca10f50d0c92b312a12a65ade928d
Gergo Kaszas <gergo.kaszas@outlook.com>
2024-08-31 20:20:54 +1000

    Create Expression.cs

---
+++ Expression/Expression.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterNumberSystem.Expression
+{
+    public class Expression
+    {
+        public List<IExpressionPart> Parts = new();
+
+        public IExpressionValue Evaluate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public (IExpressionValue, Expression) Simplify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
