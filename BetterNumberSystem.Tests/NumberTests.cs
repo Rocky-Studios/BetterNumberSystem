2542352366af976c0ee51007ffbf7509e87cde4f
Gergo Kaszas <gergo.kaszas@outlook.com>
2024-09-14 10:50:09 +1000

    Celsius to Kelvin test, summaries

--- BetterNumberSystem.Tests/NumberTests.cs
+++ BetterNumberSystem.Tests/NumberTests.cs
@@ -1,33 +1,55 @@
-using NUnit.Framework;
-
 namespace BetterNumberSystem.Tests;
 
-public class Tests
+public class NumberTests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
 
-    
+    /// <summary>
+    ///     Test if the conversion method updates the units correctly
+    /// </summary>
     [Test]
     public void NumericConvertTest()
     {
-        Number number = new Number(1.05, UnitManager.Units["Metre"]);
+        Number number = new(1.05, UnitManager.Units["Metre"]);
         Assert.AreEqual(number.Convert("Millimetre").NumericValue, 1050);
     }
-    
+
+    /// <summary>
+    ///     Test if the number is converted to a string correctly
+    /// </summary>
     [Test]
     public void TextConvertTest()
     {
-        Number number = new Number(1.05, UnitManager.Units["Metre"]);
+        Number number = new(1.05, UnitManager.Units["Metre"]);
         Assert.AreEqual(number.Convert("Millimetre").ToString(), "1050mm");
     }
-    
+
+    /// <summary>
+    ///     Test if the conversion from Celsius to Kelvin is correct
+    /// </summary>
+    [Test]
+    public void CelsiusToKelvinTest()
+    {
+        Number number = new(25, UnitManager.Units["Celsius"]);
+        Assert.AreEqual(number.Convert("Kelvin").NumericValue, 298.15);
+    }
+
+    /// <summary>
+    ///     Test if the number is parsed correctly
+    /// </summary>
     [Test]
     public void ParseTest()
     {
         Number number = Number.Parse("1.05m");
         Assert.AreEqual(number.ToString(), "1.05m");
+    }
+
+    /// <summary>
+    ///     Test whether converting the number to a string and then parsing it back results in the same number
+    /// </summary>
+    [Test]
+    public void ReverseParseTest()
+    {
+        Number number = new(1.05, UnitManager.Units["Metre"]);
+        Assert.AreEqual(Number.Parse(number.ToString()), number);
     }
 }
