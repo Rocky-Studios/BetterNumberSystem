12484de1399476479957e1e8f2b5eb1d460e34b6
Gergo Kaszas <gergo.kaszas@outlook.com>
2024-09-16 19:05:35 +1000

    SI Defining Constants NOTE: Missing units from the values

--- Expression/Pronumeral.cs
+++ Expression/Pronumeral.cs
@@ -52,6 +52,8 @@ namespace BetterNumberSystem.Expression
         public string Symbol { get; set; }
         public string Name { get; set; }
         public IExpressionValue Value { get; set; }
+
+        #region Mathematical Constants
         /// <summary>
         /// A circle's circumference divided by its diameter
         /// </summary>
@@ -70,5 +72,75 @@ namespace BetterNumberSystem.Expression
             Symbol = "e",
             Value = new Number(MathF.E)
         };
+        #endregion
+
+        #region SI Defining Constants
+        // https://en.wikipedia.org/wiki/International_System_of_Units#SI_defining_constants
+        /// <summary>
+        ///     The hyperfine transition frequency of caesium-133
+        /// </summary>
+        public static Constant HYPERFINE_TRANSITION_FREQUENCY_OF_CAESIUM = new()
+        {
+            Name = "Hyperfine Transition Frequency of Cs-133",
+            Symbol = "ΔνCs",
+            Value = new Number(9192631770)
+        };
+        /// <summary>
+        ///     The speed of light in a vacuum
+        /// </summary>
+        public static Constant SPEED_OF_LIGHT = new()
+        {
+            Name = "Speed of Light",
+            Symbol = "c",
+            Value = new Number(299792458)
+        };
+        /// <summary>
+        ///     The ground state hyperfine splitting frequency of caesium-133
+        /// </summary>
+        public static Constant PLANCK_CONSTANT = new()
+        {
+            Name = "Planck Constant",
+            Symbol = "h",
+            Value = new Number(6.62607015e-34)
+        };
+        /// <summary>
+        ///     The charge of a single electron
+        /// </summary>
+        public static Constant ELEMENTARY_CHARGE = new()
+        {
+            Name = "Elementary Charge",
+            Symbol = "e",
+            Value = new Number(1.602176634e-19)
+        };
+        /// <summary>
+        ///     The mass of a single electron
+        /// </summary>
+        public static Constant BOLTZMANN_CONSTANT = new()
+        {
+            Name = "Boltzmann Constant",
+            Symbol = "k",
+            Value = new Number(1.380649e-23)
+        };
+        /// <summary>
+        ///     The number of constituent particles per mole
+        /// </summary>
+        public static Constant AVOGADRO_CONSTANT = new()
+        {
+            Name = "Avogadro Constant",
+            Symbol = "NA",
+            Value = new Number(6.02214076e23)
+        };
+        /// <summary>
+        ///     The luminous efficacy of monochromatic radiation of frequency 540 THz
+        /// </summary>
+        public static Constant LUMINOUS_EFFICACY = new()
+        {
+            Name = "Luminous Efficacy",
+            Symbol = "Kcd",
+            Value = new Number(683)
+        };
+
+        #endregion
+
     }
 }
