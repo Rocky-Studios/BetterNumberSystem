967e01df6a296b817a7dc4a03b248ed7725cce1e
Gergo Kaszas <gergo.kaszas@outlook.com>
2024-09-20 06:51:31 +1000

    Add ToString to PronumeralCollection

--- Expression/PronumeralCollection.cs
+++ Expression/PronumeralCollection.cs
@@ -3,4 +3,10 @@
 /// <summary>
 /// A collection of pronumerals and their exponents
 /// </summary>
-public class PronumeralCollection : List<(Pronumeral, int)>;
+public class PronumeralCollection : List<(Pronumeral, int)>
+{
+    public override string ToString()
+    {
+        return string.Join("", this.Select(pronumeral => pronumeral.Item1 + (pronumeral.Item2 > 1 ? pronumeral.Item2.ToString() : "")));
+    }
+}
